74c1a5c0da81715c6cf78ca6ba59024a505165c5
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 14:27:19 +0000

    fix tests

---
+++ Controller/BackendController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\CashManagement
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\CashManagement\Controller;
+
+use phpOMS\Contract\RenderableInterface;
+use phpOMS\Message\RequestAbstract;
+use phpOMS\Message\ResponseAbstract;
+use phpOMS\Views\View;
+
+/**
+ * Calendar controller class.
+ *
+ * @package Modules\CashManagement
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ * @codeCoverageIgnore
+ */
+final class BackendController extends Controller
+{
+}
---
+++ Controller/Controller.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\CashManagement
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\CashManagement\Controller;
+
+use phpOMS\Module\ModuleAbstract;
+
+/**
+ * Calendar controller class.
+ *
+ * @package Modules\CashManagement
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+class Controller extends ModuleAbstract
+{
+    /**
+     * Module path.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const PATH = __DIR__ . '/../';
+
+    /**
+     * Module version.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const VERSION = '1.0.0';
+
+    /**
+     * Module name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const NAME = 'CashManagement';
+
+    /**
+     * Module id.
+     *
+     * @var int
+     * @since 1.0.0
+     */
+    public const ID = 1008400000;
+
+    /**
+     * Providing.
+     *
+     * @var string[]
+     * @since 1.0.0
+     */
+    public static array $providing = [];
+
+    /**
+     * Dependencies.
+     *
+     * @var string[]
+     * @since 1.0.0
+     */
+    public static array $dependencies = [];
+}
