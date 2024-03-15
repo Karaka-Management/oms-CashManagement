9d0b52fa1de13e36b9ea89d83fd572e42668ecf6
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-15 20:24:38 +0000

    code fixes

---
+++ Models/CashRegister.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\CashManagement\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\CashManagement\Models;
+
+/**
+ * Cash register.
+ *
+ * @package Modules\CashManagement\Models
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+class CashRegister
+{
+}
---
+++ Models/NullCashRegister.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\CashManagement\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\CashManagement\Models;
+
+/**
+ * Null model.
+ *
+ * @package Modules\CashManagement\Models
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+class NullCashRegister extends CashRegister
+{
+}
