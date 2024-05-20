eb9ced7fd9ee818da3e496756db14b34bc4f31b6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:07 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\CashManagement
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\CashManagement\Controller;
  * CashManagement api controller class.
  *
  * @package Modules\CashManagement
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Models/NullCashRegister.php
+++ Models/NullCashRegister.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\CashManagement\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\CashManagement\Models;
  * Null model.
  *
  * @package Modules\CashManagement\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
