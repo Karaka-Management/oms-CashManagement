8ac7510a4006aa8fed1367954e79f00fa2e93e1a
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-10-11 19:17:40 +0000

    bug fixes

---
+++ Theme/Backend/Lang/Navigation.de.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.2
+ *
+ * @package   Modules\Localization
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.2
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+return ['Navigation' => [
+    'CashManagement' => 'Cash Management',
+]];
